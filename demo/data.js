a51ce874d1818698a17504f56a6a096a95965597
lizhiguo <1620960266@qq.com>
2016-08-18 23:53:06 +0800

    demo

---
+++ demo/data.js
@@ -0,0 +1,5 @@
+var data={
+	"categories":["衬衫","上衣","短袖","裤子"],
+	"value":[200,250,70,900]
+}
+//var data={"list":[{"name":"衬衫","value":"200"},{"name":"裤子","value":"400"},{"name":"上衣","value":"300"},{"name":"短袖","value":"50"}]}
